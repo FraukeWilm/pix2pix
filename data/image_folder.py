10cdd0fee3c216ab91ce91e404ff8884c659aae5
wilm <frauke.wilm@fau.de>
2023-08-22 13:49:07 +0200

    Locate training files in provided csv table

--- data/image_folder.py
+++ data/image_folder.py
@@ -5,6 +5,10 @@ so that this class can load images from both current directory and its subdirect
 """
 
 import torch.utils.data as data
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+import glob
 
 from PIL import Image
 import os
@@ -23,10 +27,11 @@ def is_image_file(filename):
 def make_dataset(dir, max_dataset_size=float("inf")):
     images = []
     assert os.path.isdir(dir), '%s is not a valid directory' % dir
+    slides = pd.read_csv('data/scc_dataset.csv', delimiter=";")
 
     for root, _, fnames in sorted(os.walk(dir)):
         for fname in fnames:
-            if is_image_file(fname):
+            if slides.loc[slides.Slide == fname[:6]].Dataset.values.__contains__('train'):
                 path = os.path.join(root, fname)
                 images.append(path)
     return images[:min(max_dataset_size, len(images))]
