2efb233ba1d7dd2298e77334310eb9cf4e8ba73f
wilm <frauke.wilm@fau.de>
2024-05-24 16:19:22 +0200

    adapt test script to WSIs, remove website visualization

--- test.py
+++ test.py
@@ -30,8 +30,10 @@ import os
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
-from util.visualizer import save_images
-from util import html
+from util.util import tensor2im, save_image
+import ntpath
+from tqdm import tqdm
+from stitching import stitch_all
 
 try:
     import wandb
@@ -49,32 +51,32 @@ if __name__ == '__main__':
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
+    model.setup(opt)          # regular setup: load and print networks; create schedulers
 
     # initialize logger
     if opt.use_wandb:
         wandb_run = wandb.init(project=opt.wandb_project_name, name=opt.name, config=opt) if not wandb.run else wandb.run
         wandb_run._label(repo='CycleGAN-and-pix2pix')
 
-    # create a website
-    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
-    if opt.load_iter > 0:  # load_iter is 0 by default
-        web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
-    print('creating web directory', web_dir)
-    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
     # test with eval mode. This only affects layers like batchnorm and dropout.
     # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
     # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
+    #stitch_all(opt)
     if opt.eval:
         model.eval()
-    for i, data in enumerate(dataset):
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
+    for i, data in tqdm(enumerate(dataset)):
+        #if i >= opt.num_test:  # only apply our model to opt.num_test images.
+        #    break
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
         img_path = model.get_image_paths()     # get image paths
-        if i % 5 == 0:  # save images to an HTML file
-            print('processing (%04d)-th image... %s' % (i, img_path))
-        save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize, use_wandb=opt.use_wandb)
-    webpage.save()  # save the HTML
+        im = tensor2im(visuals['fake'])
+        short_path = ntpath.basename(data['A_paths'][0])
+        name = os.path.splitext(short_path)[0]
+        image_name = '%s_%s_%s.png' % (name, int(data['x']), int(data['y']))
+        save_path = os.path.join(opt.results_dir, name, image_name)
+        if not os.path.isdir(os.path.join(opt.results_dir, name)):
+            os.mkdir(os.path.join(opt.results_dir, name))
+        save_image(im, save_path, aspect_ratio=1.0)
+    stitch_all(opt)
